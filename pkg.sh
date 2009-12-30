146c1410a059a6fa3f712a334f879a84c8ae025d
simpsonjulian <simpsonjulian@gmail.com>
2009-12-30 11:21:18 +0000

    Now it's almost lintian friendly!

--- pkg.sh
+++ pkg.sh
@@ -1,8 +1,18 @@
-rm -r tmp
+set -e
+sudo rm -r tmp
 mkdir tmp
-cp -r dpkg/DEBIAN tmp
+cp -r debian/DEBIAN tmp
 mkdir -p tmp/usr/bin/
 cp bin/giternal tmp/usr/bin/
+chmod 0755 tmp/usr/bin/giternal
 mkdir -p tmp/usr/lib/ruby/1.8
+mkdir -p tmp/usr/share/doc/giternal
+mkdir -p tmp/usr/share/man/man1
+cp debian/giternal.man tmp/usr/share/man/man1/giternal.1
+gzip -9 tmp/usr/share/man/man1/giternal.1
+cp LICENSE tmp/usr/share/doc/giternal
+cp debian/copyright tmp/usr/share/doc/giternal
 cp -r lib/* tmp/usr/lib/ruby/1.8
-dpkg-deb -b tmp tmp/giternal.deb
+sudo chown -R root:root tmp/*
+dpkg-deb -b tmp giternal.deb
+scp giternal.deb 192.168.1.6:/tmp/giternal.deb
